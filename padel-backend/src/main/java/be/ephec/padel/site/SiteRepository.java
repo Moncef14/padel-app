bc7f48f4e014b67fa5a66c3833508f7a26d33907
Moncef Ouassal <moncef.ouassal@hotmail.com>
2026-05-25 23:15:03 +0200

    Add SiteRepository

---
+++ padel-backend/src/main/java/be/ephec/padel/site/SiteRepository.java
@@ -0,0 +1,10 @@
+package be.ephec.padel.site;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SiteRepository extends JpaRepository<Site, Long> {
+
+    List<Site> findByActifTrue();
+}

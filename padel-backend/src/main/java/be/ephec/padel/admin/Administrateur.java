66114999ff427a00736ca87a7b7b953d4fc1ec7c
Moncef Ouassal <moncef.ouassal@hotmail.com>
2026-05-31 17:00:40 +0200

    Add Administrateur feature - Entity, Repository, Service, Controller

---
+++ padel-backend/src/main/java/be/ephec/padel/admin/Administrateur.java
@@ -0,0 +1,35 @@
+package be.ephec.padel.admin;
+
+import be.ephec.padel.site.Site;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "administrateurs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Administrateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String motDePasse;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleAdmin role;
+
+    @ManyToOne
+    @JoinColumn(name = "site_id")
+    private Site site;
+}
---
+++ padel-backend/src/main/java/be/ephec/padel/admin/RoleAdmin.java
@@ -0,0 +1,5 @@
+package be.ephec.padel.admin;
+
+public enum RoleAdmin {
+    ADMIN_SITE, ADMIN_GLOBAL
+}

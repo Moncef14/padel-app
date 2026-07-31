62ef9dad144169ae0370624356b294cc22a7c136
Moncef Ouassal <moncef.ouassal@hotmail.com>
2026-08-01 00:30:54 +0200

    fix: configure fr-BE locale for consistent date format across app

--- padel-frontend/src/app/app.config.ts
+++ padel-frontend/src/app/app.config.ts
@@ -1,15 +1,23 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { routes } from './app.routes';
 import { jwtInterceptor } from './interceptors/jwt-interceptor';
+
+registerLocaleData(localeFr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([jwtInterceptor]))
+    provideHttpClient(withInterceptors([jwtInterceptor])),
+    { provide: MAT_DATE_LOCALE, useValue: 'fr-BE' },
+    { provide: LOCALE_ID, useValue: 'fr-BE' }
   ]
 };
